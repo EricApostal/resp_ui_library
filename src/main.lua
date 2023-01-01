f6d175757c48820bf2dacfb471a49917b0630abb
Eric A <ehapostal04@gmail.com>
2023-01-01 17:49:31 -0500

    allow main.lua to actually be callable

--- src/main.lua
+++ src/main.lua
@@ -1,7 +1,7 @@
 --[[ 
 Main UI builder :)
 ]]
-print('MAIN FUNCTION RAN REEEE')
+
 local roact = {}
 for _, file in listfiles('repos\\roact', true) do  -- Set the second argument to `true` to include subdirectories
     if not file:match("%.%a+$") then  -- Skip directories
@@ -10,27 +10,6 @@ for _, file in listfiles('repos\\roact', true) do  -- Set the second argument to
     end
 end
 
-
---[[
-    That is my hacky way of loading roact locally
-    pretty fast tho
-]]
-
-for _,v in roact do
-    print(v)
+function main()
+    print("main lua file called!")
 end
-
--- ======================================================== -- 
-
-local LocalPlayer = game:GetService("Players").LocalPlayer
-
--- Create our virtual tree describing a full-screen text label.
-local tree = roact.createElement("ScreenGui", {}, {
-	Label = roact.createElement("TextLabel", {
-		Text = "Hello, world!",
-		Size = UDim2.new(1, 0, 1, 0),
-	}),
-})
-
--- Turn our virtual tree into real instances and put them in PlayerGui
-roact.mount(tree, LocalPlayer.PlayerGui, "HelloWorld")
