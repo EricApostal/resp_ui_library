e630b3505d0ce9632a401ded7586949d967ceceb
Eric A <ehapostal04@gmail.com>
2023-01-02 12:32:11 -0500

    added updater, now takes all git urls, bugfixes

--- installer.lua
+++ installer.lua
@@ -1,100 +1,213 @@
---[[
-    Grabs and builds the script + depends automatically
-    intentionally modular for you skiddies out there :)
-]]
+-- Get the HttpService
+local http_service = game:GetService("HttpService")
 
---[[
-this script already needs a rewrite:
-	- separate function for indexing into subdirectories ->
-		returns table w/paths, then iterates those paths and downlaods in a 
-		much more simple script.
+-- Main function to build the repository
+function build_repository(repository_url, folder_name)
+	local url = repository_url
 
-	
+	if not string.match(url, "api.github.com") then
+		-- so you can just drop any github url in and it works :)
 
-]]
+		url = string.gsub(url, "www.github.com/", "api.github.com/repos/")
+		url = string.gsub(url, "http://github.com/", "https://api.github.com/repos/")
+		url = string.gsub(url, "https://github.com/", "https://api.github.com/repos/")
+		url = string.gsub(url, "/tree/master/", "/contents/")
 
-function build_repository(base_repo: string, folder_name: string, extra_path: string, is_recursive: boolean) 
-	--[[
-		- Recursively iterates through github repo
-		- It has some strange params, but generally it's not too bad
-		- I may make a function that generates an array of all information, then a second one 
-			to actually download that info, so I could make an accurate progress bar.
-	--]]
-	if not is_recursive then is_recursive = false end -- nil and false work very strangely in lua...
-
-	local folder_path = 'repos\\' .. folder_name
-	local last_update_path = folder_path .. "\\last_update.txt"
-
-	makefolder("repos")
-	makefolder(folder_path)
-
-	--[[
-		- It's important to note that "is_recursive" is only true when descendend into subdirectories
-		- It's generally better to iterate this way so I don't have to maintain 2 functions that
-		essentially do the same thing
-	]]
-
-	if not is_recursive then
-		print("Installing " .. folder_name .. "...")
-		url = base_repo .. "contents/" .. (extra_path or '')
-	else 
-		url = base_repo
+		print(string.format("string did not match url, new url = %s", url))
 	end
 
-	local needs_update_resp = game:GetService("HttpService"):JSONDecode(http.request({
-		Url = base_repo .. "branches/master",
-		Method = "GET"
-		}).Body )
+    -- Print a message indicating that the repository is being built
+    print(string.format("Now building repository for %s", folder_name))
 
-	local needs_update = false
-	if isfile(folder_path .. "\\last_update.txt") then
-		if readfile(last_update_path) ~= needs_update_resp.commit.commit.committer.date then
-			print("Repository " .. folder_name .. " needs to be updated, one moment!")
-			delfolder(folder_path)
-			needs_update = true
-		else
-			print("Dependency up to date!")
-			print(readfile(last_update_path))
-			print(needs_update_resp.commit.commit.author.date)
-		end
-	end
+    -- Create the "repos" directory if it does not exist
+    makefolder("repos")
 
-	local a = http.request({
+    -- Create a subdirectory within the "repos" directory with the specified name
+    makefolder(string.format("repos/%s", folder_name))
+
+    -- Generate an index of the repository's files and directories
+    local repository_index = generate_index(url, folder_name)
+
+    -- Download all of the files in the repository
+    download_files(repository_index, folder_name, url)
+
+    -- Print a message indicating that the repository has been built
+    print("Repository built!")
+end
+
+function send_request(url)
+ -- https://api.github.com/repos/SirTZN/resp_ui_library/branches/master
+
+	local response = http.request({
 	Url = url,
-	Method = "GET"
+	Method = "GET",
+	Headers = {
+		["Authorization"] = ""
+	}
 	})
-
-	local response = a.Body
-	local json_body = game:GetService("HttpService"):JSONDecode(response)
-	for _, v in json_body do
-		if v.name == nil then warn(v) end
-			if needs_update or (table.find(listfiles(folder_path), folder_path .. "\\" .. v.name) == nil) then
-				if v.type == 'file' then
-					local content = http.request({
-					Url = v.download_url,
-					Method = "GET"
-					}).Body
-					writefile(folder_path .. '\\' .. v.name, content)
-				elseif v.type == 'dir' then
-					makefolder(folder_path .. '\\' .. v.name)
-					build_repository(v.url, folder_name .. '\\' .. v.name, nil, true)
-				end
-				
-			else
-				-- print("Found " .. folder_path .. "\\" .. v.name)
-			end
+	if not response.StatusCode == 200 then
+		warn(string.format("Request returned with code %s!", response.StatusCode))
 	end
+	return response
+end
+
+function get_base_url(url)
+	-- returns user/repo_name so it can be used for other api hits, while still allowing extendable urls
+	-- ei: you can still pull from sirtzn/ui_lib/src, and it will pull sirtzn/ui_lib as well
+
+	local api_base_stripped = string.gsub(url, "https://", "")
+	local api_base_stripped = string.gsub(api_base_stripped, "http://", "")
+	local api_base_stripped = string.gsub(api_base_stripped, "api.github.com/repos/", "")
+	local api_base_stripped = string.gsub(api_base_stripped, "www.github.com/", "")
 	
-	if not is_recursive then
-		print(needs_update_resp)
-		writefile(last_update_path, needs_update_resp.commit.commit.author.date)
-		print("Installed " .. folder_name .. "!")
+	local split_url = string.split(api_base_stripped, '/')
+
+	local _r = string.format("%s/%s", split_url[1], split_url[2])
+	print(string.format("base url is: %s", _r))
+	return _r
+end
+
+function get_folder(full_path)
+	-- strips the name of the item off, so I can accurately index the folder
+
+    local working_directory = "repos/"
+    local path_split = string.split(full_path, '/')
+    table.remove(path_split, #path_split)
+
+    -- Iterate through the segments of the directory path and create each directory if it does not exist
+    for _, segment in ipairs(path_split) do
+        working_directory = string.format("%s%s/", working_directory, segment)
+    end
+
+    -- Remove the trailing slash from the directory path
+    working_directory = working_directory:sub(1, #working_directory - 1)
+	return working_directory
+end
+
+function needs_update(local_repository_base, repository_url)
+	-- checks if it needs to be installed or updated
+	local last_update_path = string.format("%s/last_update.txt", "repos/" .. local_repository_base)
+	if not isfile( last_update_path ) then
+		print( string.format("(%s) Can't find last update, assuming first download", local_repository_base ))
+		return true
+	end
+
+	if get_last_commit_time(repository_url) == readfile(last_update_path) then
+		-- does not need to update (same version)
+		return false
+	else
+		-- does need to update (different version)
+		return true
 	end
 end
 
-print("Checking dependencies...")
-build_repository("https://api.github.com/repos/Roblox/roact/", "roact", "src?ref=master")
-build_repository("https://api.github.com/repos/SirTZN/resp_ui_library/", "resp_ui_lib")
-print("Installed, running script now...")
-local main_script = loadfile('repos\\resp_ui_lib\\src\\main.lua')
-print(main_script)
+function get_last_commit_time(repository_url)
+
+	local last_commit_url = "https://api.github.com/repos/" .. get_base_url(repository_url) .. "/branches/master"
+	local last_commit_str = send_request( last_commit_url ).Body
+	local last_commit_time = http_service:JSONDecode(last_commit_str).commit.commit.committer.date
+
+	return last_commit_time
+end
+-- Function to download all of the files in the repository
+function download_files(repository_index, local_repository_base, repository_url)
+    -- Iterate through the list of files in the repository
+	if not needs_update(local_repository_base, repository_url) then
+		print("Repo is already up to date!")
+		return true
+	end
+
+    for i, file_data in ipairs(repository_index) do
+        -- Print a message indicating the progress of the download
+        print(string.format("%d/%d items downloaded!", i, #repository_index))
+
+        -- Create the directory structure for the file if it does not exist
+        create_directory(file_data.path)
+
+        -- Download the file
+        download_file(file_data)
+    end
+
+	-- Create file indicating the last repository update
+	local last_commit_time = get_last_commit_time(repository_url)
+
+	local path_to_write = string.format("repos/%s/last_update.txt", local_repository_base)
+	writefile(path_to_write, last_commit_time)
+end
+
+-- Function to create the directory structure for a file if it does not exist
+function create_directory(file_path)
+
+	-- retrieves the folder name, removing the file
+	-- ei: repos/roact/hello.lua -> repos/roact
+	local working_directory = get_folder(file_path)
+
+    -- Create the directory if it does not exist
+    if not isfolder(working_directory) then
+        makefolder(working_directory)
+
+        -- If the directory still does not exist after being created, throw an error
+        if not isfolder(working_directory) then
+            error("No folder was made, you done messed something up -_-")
+        end
+    end
+end
+
+-- Function to download a file
+function download_file(file_data)
+    -- Make a request to the download URL of the file
+
+	local response = send_request(file_data.download_url)
+    -- Get the content of the file
+    local file_content = response.Body
+
+    -- Write the file to the repository directory
+    writefile(string.format("repos/%s", file_data.path), file_content)
+end
+
+-- Function to generate an index of the repository's files and directories
+function generate_index(repository_url, folder_name)
+    -- Initialize a table to store the index
+    local repository_indexes = {}
+
+    -- Make a request to the repository URL
+    local response = send_request(repository_url)
+
+    -- If the request is not successful, print a warning message
+    if response.StatusCode ~= 200 then
+        warn(string.format("Status code %d was thrown when accessing URL %s",
+            response.StatusCode, repository_url))
+    end
+
+    -- Decode the JSON response from the request
+    local data = http_service:JSONDecode(response.Body)
+
+    -- Iterate through the list of files and directories in the repository
+    for _, file_data in data do
+        -- If the current entry is a file, add it to the index
+        if file_data.type == "file" then
+            table.insert(repository_indexes, {
+                name = file_data.name,
+                download_url = file_data.download_url,
+                path = string.format("%s/%s", folder_name, file_data.name)
+            })
+        -- If the current entry is a directory, recursively generate an index for it
+        elseif file_data.type == "dir" then
+            local subdirectory_name = file_data.name
+            makefolder(string.format("%s/%s/", folder_name, subdirectory_name))
+            local subdirectory_data = generate_index(
+                string.format("%s/%s", repository_url, file_data.name), folder_name)
+            -- Add the files and directories in the subdirectory to the index, with their paths modified to reflect the subdirectory
+            for _, subdirectory_file_data in subdirectory_data do
+                local _fdata = subdirectory_file_data
+                _fdata.path = string.format("%s/%s/%s", folder_name, subdirectory_name, _fdata.name)
+                table.insert(repository_indexes, _fdata)
+            end
+        end
+    end
+    return repository_indexes
+end
+
+print("Building roact repository...")
+build_repository("https://github.com/Roblox/roact/tree/master/src", "roact")
+print("Roact has been downloaded!")
